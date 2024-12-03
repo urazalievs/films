79935db80399bc9dc148ec76dcbfe5a4e79e1d3f
urazalievs <sultaurazaliev1@gmail.com>
2024-12-03 18:22:59 +0300

    commit add

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 "use strict"
 
+// Элементы
+const main = document.getElementsByClassName("main")[0];
+const movieTitle = document.getElementsByClassName("movieTitle")[0];
+const simTitle = document.getElementsByClassName("movieTitle")[1];
+
+const semularMovie = document.getElementsByClassName("semularMovie")[0];
+const movie = document.getElementsByClassName("movie")[0];
+
+
 // кнопки
 const themeChange = document.getElementById("themeChange");
 const searchBtn = document.getElementById("searchBtn")
@@ -40,13 +49,54 @@ async function sendRequest(url, method, data) {
 }
 
 
+
+
 // Поиск фильма
 async function findMovie(){
     let search = document.getElementsByName("search")[0].value;
-    let data = { apikey: "1b7ff984", t: search };
-
+    let data = { apikey: "d6d0aa42", t: search };
+    let loader = document.getElementsByClassName("loader")[0]
+    loader.style.display="block";
     let response = await sendRequest("https://www.omdbapi.com/" , "GET", data);
+    loader.style.display="none";
 
-    return console.log(response);
+    if(response.Response == "False"){
+        movie.style.display="none"
+        main.style.display="block"
+        movieTitle.style.display="block"
+        movieTitle.innerHTML = `${response.Error}`
+        console.log(response);
+        
+    }else{
+       showMovie(response)
+        
+    }
     
 }
+
+function showMovie(movie){
+    main.style.display="block"
+    movieTitle.style.display="block"
+    document.getElementsByClassName("movie")[0].style.display="flex";
+    document.getElementById("movieImg").style.backgroundImage = `url(${movie.Poster})`;
+    movieTitle.innerHTML = `${movie.Title}`
+    const movieDesc = document.getElementsByClassName("movieDescription")[0];
+    movieDesc.innerHTML ="";
+    let params = [
+        "imdbRating", "Year", "Released", "Genre", "Country", "Language", "Director", "Writer", "Actors", 
+    ]
+    
+    params.forEach((key)=>{
+        movieDesc.innerHTML+= `
+        <div class="desc">
+            <span class="title">${key}</span>
+            <span class="subTitle">${movie[key]}</span>
+          </div>
+        `
+    })
+    console.log(movie);
+    
+}
+
+
+
